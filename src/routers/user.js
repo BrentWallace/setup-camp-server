c1bcd60abddae1de8b83b1768f754ccab77d8878
BrentWallace <bwallace@carefieldliving.com>
2020-07-05 08:06:15 -0400

    added logout routes and auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const auth = require('../middleware/auth');
 const User = require('../models/user');
 
 const router = new express.Router();
 
+// Get all users
 router.get('/users', async (req, res) => {
     const users = await User.find()
 });
 
+// Create user
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
@@ -16,6 +19,41 @@ router.post('/users', async (req, res) => {
         res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
+    }
+})
+
+// Login user
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+// Logout user
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save();
+        res.send()
+    } catch (e) {
+        res.status(500).send;
+    }
+})
+
+// Logout user all tokens
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
