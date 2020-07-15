ebc656e2ef545506e503f2e31dfc904df5628ed5
BrentWallace <bwallace@carefieldliving.com>
2020-07-14 22:04:55 -0400

    Signup/Login/Logout routes working

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,59 +7,46 @@ const router = new express.Router();
 
 // Get all users
 router.get('/users', async (req, res) => {
-    const response = await User.find();
-    let users = response.map((user) => {
-        return user.toJSON();
-    })
-    res.status(200).send(users);
+  const response = await User.find();
+  let users = response.map((user) => {
+    return user.toJSON();
+  })
+  res.status(200).send(users);
 });
 
 // Create user
 router.post('/users', async (req, res) => {
-    const user = new User(req.body);
-    try {
-        await user.save()
-        const token = await user.generateAuthToken()
-        res.status(201).send({ user, token });
-    } catch (e) {
-        res.status(400).send(e);
-    }
+  const user = new User(req.body);
+  try {
+    await user.save()
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
 })
 
 // Login user
 router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({ user, token })
-    } catch (e) {
-        console.log(e)
-        res.status(400).send(e)
-    }
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (e) {
+    console.log(e)
+    res.status(400).send(e)
+  }
 })
 
 // Logout user
-router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
-        await req.user.save();
-        res.send()
-    } catch (e) {
-        res.status(500).send;
-    }
-})
-
-// Logout user all tokens
-router.post('/users/logoutAll', auth, async (req, res) => {
-    try {
-        req.user.tokens = []
-        await req.user.save()
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.post('/users/logout', async (req, res) => {
+  try {
+    const query = { _id: req.body._id }
+    const response = await User.findOneAndUpdate(query, { tokens: [] })
+    res.status(200).send()
+  } catch (e) {
+    res.status(500).send;
+  }
+});
 
 module.exports = router
